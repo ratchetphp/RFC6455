8944361dbe69c82da22009b267950b97c92f1650
Matt Bonneau <matt@bonneau.net>
2019-12-10 19:22:57 -0500

    Update to latest react/socket and drop react/http for tests

--- tests/ab/startServer.php
+++ tests/ab/startServer.php
@@ -7,49 +7,60 @@ require_once __DIR__ . "/../bootstrap.php";
 
 $loop   = \React\EventLoop\Factory::create();
 
-$socket = new \React\Socket\Server($loop);
-$server = new \React\Http\Server($socket);
+$socket = new \React\Socket\Server('127.0.0.1:9001', $loop);
 
 $closeFrameChecker = new \Ratchet\RFC6455\Messaging\CloseFrameChecker;
 $negotiator = new \Ratchet\RFC6455\Handshake\ServerNegotiator(new \Ratchet\RFC6455\Handshake\RequestVerifier);
 
 $uException = new \UnderflowException;
 
-$server->on('request', function (\React\Http\Request $request, \React\Http\Response $response) use ($negotiator, $closeFrameChecker, $uException) {
-    $psrRequest = new \GuzzleHttp\Psr7\Request($request->getMethod(), $request->getPath(), $request->getHeaders());
-
-    $negotiatorResponse = $negotiator->handshake($psrRequest);
-
-    $response->writeHead(
-        $negotiatorResponse->getStatusCode(),
-        array_merge(
-            $negotiatorResponse->getHeaders(),
-            ["Content-Length" => "0"]
-        )
-    );
-
-    if ($negotiatorResponse->getStatusCode() !== 101) {
-        $response->end();
-        return;
-    }
-
-    $parser = new \Ratchet\RFC6455\Messaging\MessageBuffer($closeFrameChecker, function(MessageInterface $message) use ($response) {
-        $response->write($message->getContents());
-    }, function(FrameInterface $frame) use ($response, &$parser) {
-        switch ($frame->getOpCode()) {
-            case Frame::OP_CLOSE:
-                $response->end($frame->getContents());
-                break;
-            case Frame::OP_PING:
-                $response->write($parser->newFrame($frame->getPayload(), true, Frame::OP_PONG)->getContents());
-                break;
+$socket->on('connection', function (React\Socket\ConnectionInterface $connection) use ($negotiator, $closeFrameChecker, $uException) {
+    $headerComplete = false;
+    $buffer = '';
+    $parser = null;
+    $connection->on('data', function ($data) use ($connection, &$parser, &$headerComplete, &$buffer, $negotiator, $closeFrameChecker, $uException) {
+        if ($headerComplete) {
+            $parser->onData($data);
+            return;
         }
-    }, true, function() use ($uException) {
-        return $uException;
-    });
 
-    $request->on('data', [$parser, 'onData']);
+        $buffer .= $data;
+        $parts = explode("\r\n\r\n", $buffer);
+        if (count($parts) < 2) {
+            return;
+        }
+        $headerComplete = true;
+        $psrRequest = \GuzzleHttp\Psr7\parse_request($parts[0] . "\r\n\r\n");
+        $negotiatorResponse = $negotiator->handshake($psrRequest);
+
+        $negotiatorResponse = $negotiatorResponse->withAddedHeader("Content-Length", "0");
+
+        $connection->write(\GuzzleHttp\Psr7\str($negotiatorResponse));
+
+        if ($negotiatorResponse->getStatusCode() !== 101) {
+            $connection->end();
+            return;
+        }
+
+        $parser = new \Ratchet\RFC6455\Messaging\MessageBuffer($closeFrameChecker,
+            function (MessageInterface $message) use ($connection) {
+                $connection->write($message->getContents());
+            }, function (FrameInterface $frame) use ($connection, &$parser) {
+                switch ($frame->getOpCode()) {
+                    case Frame::OP_CLOSE:
+                        $connection->end($frame->getContents());
+                        break;
+                    case Frame::OP_PING:
+                        $connection->write($parser->newFrame($frame->getPayload(), true, Frame::OP_PONG)->getContents());
+                        break;
+                }
+            }, true, function () use ($uException) {
+                return $uException;
+            });
+
+        array_shift($parts);
+        $parser->onData(implode("\r\n\r\n", $parts));
+    });
 });
 
-$socket->listen(9001, '0.0.0.0');
 $loop->run();
